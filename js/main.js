863f8195c7fbecda6e4b8169b083cc1cd0febffb
logan18 <l.brandwood@hotmail.com>
2014-12-07 01:28:10 -0500

    fixed nav button
    -added hide-text attribute and adjusted size and placement of button

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ $(".close").on("click",function(){
 	$(this).parent().attr("data-state","close");
 	
 	});
+	
+var navTop = document.querySelector('.nav-toggle');
+document.querySelector('.nav-btn').addEventListener('click', function (e) {
+	e.preventDefault();
+	if (navTop.getAttribute('data-state') == 'expanded') {
+		navTop.setAttribute('data-state', 'collapsed');
+		this.setAttribute('data-state', 'inactive');
+	} else {
+		navTop.setAttribute('data-state', 'expanded');
+		this.setAttribute('data-state', 'active');
+	}
+});
